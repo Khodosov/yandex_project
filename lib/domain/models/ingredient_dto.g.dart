a4ad2ff997dc0cb006456e115f67c39be81b331c
Alexander Khodosov <43482216+Khodosov@users.noreply.github.com>
2021-12-10 12:37:24 +0300

    commit before merge

---
+++ lib/domain/models/ingredient_dto.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ingredient_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+IngredientDTO _$IngredientDTOFromJson(Map<String, dynamic> json) =>
+    IngredientDTO(
+      name: json['name'] as String,
+      id: json['id'] as int,
+    );
+
+Map<String, dynamic> _$IngredientDTOToJson(IngredientDTO instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'id': instance.id,
+    };
