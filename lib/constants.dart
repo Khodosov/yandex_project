61dfb9d1c9fae88f860af6c809385d4f6c0df00d
Alexander Khodosov <43482216+Khodosov@users.noreply.github.com>
2021-12-10 17:20:18 +0300

    premerge commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,4 +2,7 @@ class AppConstants {
   static const double smallRadius = 20;
 
   static const double bigRadius = 20;
+
+  static const double titleFontSize = 30;
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:yandex_project/application/navigation/navigation_bloc.dart';
+import 'package:yandex_project/presentation/screens/drink_info/drink_info_widget.dart';
 import 'package:yandex_project/presentation/screens/home/home_page.dart';
 import 'package:yandex_project/presentation/screens/settings/settings_screen.dart';
 import 'application/preferences/preferences_bloc.dart';
 import 'application/search/search_bloc.dart';
 
-void main() {
+void main()  {
   runApp(const App());
 }
 
@@ -41,11 +42,17 @@ class App extends StatelessWidget {
               progressIndicatorTheme: const ProgressIndicatorThemeData(color: Colors.black),
             ),
             darkTheme: ThemeData.dark(),
-            routes: <String, WidgetBuilder>{
-              '/home': (BuildContext context) => const HomePage(),
-              '/settings': (BuildContext context) => const SettingsScreen(),
+            onGenerateRoute: (settings) {
+              switch (settings.name) {
+                case '/home':
+                  return MaterialPageRoute(builder: (context) => const HomePage(), settings: settings);
+                case '/settings':
+                  return MaterialPageRoute(builder: (context) => const SettingsScreen(), settings: settings);
+                case '/drink':
+                  return MaterialPageRoute(builder: (context) => const DrinkInfoScreen(), settings: settings);
+              }
             },
-            home: const HomePage(),
+            initialRoute: '/home',
           );
         },
       ),
