0a8f60f87a46870efb3873858ed2b2321cc54406
Alexander Khodosov <43482216+Khodosov@users.noreply.github.com>
2021-12-17 22:20:03 +0300

    removed unused import

--- lib/application/services/application_db.dart
+++ lib/application/services/application_db.dart
@@ -1,4 +1,3 @@
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:yandex_project/application/preferences/preferences_bloc.dart';
 import 'package:yandex_project/domain/general/enums.dart';
